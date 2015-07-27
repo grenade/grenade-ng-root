1d384f600a314da1acde51c553c153f00cc5eada
Rob Thijssen <rthijssen@gmail.com>
2015-07-27 16:04:30 +0100

    yo scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grenadeNgRootApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the grenadeNgRootApp
+ */
+angular.module('grenadeNgRootApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name grenadeNgRootApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the grenadeNgRootApp
+ */
+angular.module('grenadeNgRootApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
