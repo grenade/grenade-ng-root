4618320f7c72d4255b4f3eac481e6874c67eaf0a
Rob Thijssen <rthijssen@gmail.com>
2015-07-29 08:17:34 +0100

    restore subdirs

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git clone "https://${GH_TOKEN}@${GH_REF}" pages_repo
-rm -rf pages_repo/!(.|..|.git|images|bugzoolla|cv)
+rm -rf pages_repo/*
+git --git-dir=./pages_repo/.git --work-tree=./pages_repo checkout bugzoolla
+git --git-dir=./pages_repo/.git --work-tree=./pages_repo checkout cv
 cp -r dist pages_repo
 git --git-dir=./pages_repo/.git --work-tree=./pages_repo config user.name "${GIT_NAME}"
 git --git-dir=./pages_repo/.git --work-tree=./pages_repo config user.email "${GIT_EMAIL}"
